a028dd86bd289542d983eaa319fd9db52bc2958c
Neeme Kahusk <nemee@ut.ee>
2023-05-12 10:15:03 +0300

    print doc as xml

--- tei2od/odt2xml.py
+++ tei2od/odt2xml.py
@@ -5,18 +5,23 @@ Write odt as xml
 """
 
 import argparse
+from odf.opendocument import load
 
 def returnparser():
     parser = argparse.ArgumentParser(
         description='odt files to xml'
     )
     parser.add_argument('infile')
-    parser.add_argument('outfile')
+    # parser.add_argument('outfile')
 
     return parser
 
 def main(p: argparse.ArgumentParser):
     args = p.parse_args()
+    infile = args.infile
+    doc = load(infile)
+
+    print(doc.xml())
 
 if __name__ == '__main__':
     argparser = returnparser()
