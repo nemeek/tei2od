54440eede10fafb0d8074b3fb44c67da5c51aa46
Neeme Kahusk <nemee@ut.ee>
2023-05-12 12:03:22 +0300

    pretty print doc as xml

--- tei2od/odt2xml.py
+++ tei2od/odt2xml.py
@@ -5,6 +5,8 @@ Write odt as xml
 """
 
 import argparse
+from lxml import etree
+
 from odf.opendocument import load
 
 def returnparser():
@@ -20,8 +22,9 @@ def main(p: argparse.ArgumentParser):
     args = p.parse_args()
     infile = args.infile
     doc = load(infile)
-
-    print(doc.xml())
+    ddoc = etree.fromstring(doc.xml())
+    dddoc = etree.tostring(ddoc, pretty_print=True, encoding="unicode")
+    print(dddoc)
 
 if __name__ == '__main__':
     argparser = returnparser()
